a5b7da1127f2b4d814edd1934185125d069ae5d3
Adrien Tellier <a.tellier@student.isartdigital.com>
2021-01-06 16:51:42 +0100

    Fireworks: start launch system

---
+++ UnityHDRP/Assets/Scripts/FireworkLauncher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class FireworkLauncher : MonoBehaviour
+{
+    [SerializeField]
+    private VisualEffect m_firework = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_firework.Play();
+        m_firework.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
